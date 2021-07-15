fbb0967009163099486ede2a1d72dce42a461cf3
Dauwbot <dauwprog@pm.me>
2021-07-15 11:13:08 +0200

    Extracted code to remove accentuation to it's own method

--- Program.cs
+++ Program.cs
@@ -9,32 +9,36 @@ namespace passwordCreator
     {
         static void Main(string[] args)
         {
+            RemoveAccentuationFromFile("./sourceFiles/francais.txt");
+        }
+    
+        static void RemoveAccentuationFromFile (string filePath) {
             int linesRemoved = 0;
             String line;
             try
             {
-                StreamReader sr = new StreamReader("./sourceFiles/francais.txt", Encoding.GetEncoding("iso-8859-1"));
+                StreamReader sr = new StreamReader(filePath, Encoding.GetEncoding("iso-8859-1")); // Latin 1; Western European (ISO) Encoding, otherwise it get opened in UTF8
                 string tempFile = Path.GetTempFileName();
                 StreamWriter sw = new StreamWriter(tempFile);
                 line = sr.ReadLine();
                 while (line != null)
                 {
-                    string normalizedLine = line.Normalize(NormalizationForm.FormD);
-                    int lineLength = normalizedLine.Length;
+                    string normalizedLine = line.Normalize(NormalizationForm.FormD); // https://stackoverflow.com/questions/9349608/how-to-check-if-unicode-character-has-diacritics-in-net It separate the diacritics from the preceding letter
+                    int lineLength = normalizedLine.Length; // So that we can know where we're in our line & get the last letter
                     int currentPos = 0;
                     foreach (char c in normalizedLine)
                     {
                         switch (CharUnicodeInfo.GetUnicodeCategory(c))
                         {
-                            case UnicodeCategory.UppercaseLetter:
+                            case UnicodeCategory.UppercaseLetter: // I don't want countries names or people in my password
                                 Console.WriteLine(line);
                                 linesRemoved++;
                                 break;
-                            case UnicodeCategory.DashPunctuation:
+                            case UnicodeCategory.DashPunctuation:   // so that we don't get words with a dash in it ex: demi-dieu, semi-automatique, sous-bois
                                 Console.WriteLine(line);
                                 linesRemoved++;
                                 break;
-                            case UnicodeCategory.NonSpacingMark:
+                            case UnicodeCategory.NonSpacingMark: // With the Normalize method we separate our accent from our letter
                                 Console.WriteLine(line);
                                 linesRemoved++;
                                 break;
@@ -59,14 +63,15 @@ namespace passwordCreator
                     line = sr.ReadLine();
                 }
                 sr.Close();
-                Console.WriteLine($"Removed {linesRemoved} from file");
-                File.Move(tempFile, "./sourceFiles/francais_modified.txt");
-                Console.ReadLine();
+                Console.WriteLine($"Removed {linesRemoved} lines from file");
+                File.Delete(filePath);
+                File.Move(tempFile, filePath);
             }
             catch (System.Exception e)
             {
                 Console.WriteLine("Exception: " + e.Message);
             }
-        }
+    }
+
     }
 }
