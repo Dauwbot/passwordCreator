0e30b2072bc402df2904d66867ede9f986314c81
Dauwbot <dauwprog@pm.me>
2021-07-15 09:37:19 +0200

    Enabled C# project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace passwordCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
